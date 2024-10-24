0564fe5b9cd4ab83902a36f8d66fb3309b0a5d54
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2024-10-24 09:05:31 +0200

    feat: made a proper config

---
+++ neovim/nvim/lua/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-j>", "}")
+vim.keymap.set("n", "<C-k>", "{")
+
+vim.keymap.set("v", "<C-j>", "}")
+vim.keymap.set("v", "<C-k>", "{")
---
+++ neovim/nvim/lua/set.lua
@@ -0,0 +1,42 @@
+-- for neo-tree
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+
+vim.opt.nu = true
+
+-- tabs
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.expandtab = false
+
+-- vim.g.python_recommended_style = 0
+---
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hls = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+-- vim.opt.nocompatible = true
+-- vim.opt.syntax = true
+-- filetype plugin on
