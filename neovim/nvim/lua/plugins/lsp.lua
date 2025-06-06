a1bf911c014c8710ecfc582e3c8b31706bf544f5
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-06-06 13:49:54 +0300

    really switched to french

--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -95,7 +95,7 @@ return {
 				filetypes = { "vimwiki", "markdown", "md", "pandoc", "vimwiki.markdown.pandoc", "tex", "latex" },
 				settings = {
 					ltex = {
-						language = "en-GB",
+						language = "fr",
 					},
 				},
 			})
