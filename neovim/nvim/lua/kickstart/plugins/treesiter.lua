c1b349f428af4b7d51cc24435aa4160bbed4c151
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-07-28 21:29:48 +0300

    added latex interactions with treesiter

--- neovim/nvim/lua/kickstart/plugins/treesiter.lua
+++ neovim/nvim/lua/kickstart/plugins/treesiter.lua
@@ -27,10 +27,9 @@ return { -- Highlight, edit, and navigate code
 			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 			--  If you are experiencing weird indenting issues, add the language to
 			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-			disable = { "tex", "latex" },
+			additional_vim_regex_highlighting = { "ruby", "tex", "latex" },
 		},
-		indent = { enable = true, disable = { "ruby" } },
+		indent = { enable = true, disable = { "ruby", "tex", "latex" } },
 	},
 	-- There are additional nvim-treesitter modules that you can use to interact
 	-- with nvim-treesitter. You should go explore a few and see what interests you:
