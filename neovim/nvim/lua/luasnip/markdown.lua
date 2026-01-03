c627272c3741ca4661428d10667909cb1c297d6f
Guenael Roger <guenael.mail@gmail.com>
2026-01-03 18:27:11 +0100

    snippet for markdown file

---
+++ neovim/nvim/lua/luasnip/markdown.lua
@@ -0,0 +1,6 @@
+return {
+	s({ trig = "&uarr", snippetType = "autosnippet", dscr = "An up arrow" }, t("↑")),
+	s({ trig = "&darr", snippetType = "autosnippet", dscr = "A down arrow" }, t("↓")),
+	s({ trig = "&larr", snippetType = "autosnippet", dscr = "A left arrow" }, t("←")),
+	s({ trig = "&rarr", snippetType = "autosnippet", dscr = "A right arrow" }, t("→")),
+}
