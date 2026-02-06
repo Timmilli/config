00ecdf5d3f006d18e4394addf42d7c47733f85d5
Guenael Roger <guenael.mail@gmail.com>
2026-02-06 14:11:24 +0100

    new config with more snippets

--- neovim/nvim/lua/luasnip/markdown.lua
+++ neovim/nvim/lua/luasnip/markdown.lua
@@ -3,4 +3,11 @@ return {
 	s({ trig = "&darr", snippetType = "autosnippet", dscr = "A down arrow" }, t("↓")),
 	s({ trig = "&larr", snippetType = "autosnippet", dscr = "A left arrow" }, t("←")),
 	s({ trig = "&rarr", snippetType = "autosnippet", dscr = "A right arrow" }, t("→")),
+	s({ trig = "img", snippetType = "autosnippet", dscr = "Makes a image environment" }, {
+		t({ "<div style='display: flex; justify-content: center; gap: 3rem;'>", "    <img alt='" }),
+		i(1, "Alt text"),
+		t({ "'", "         src='" }),
+		i(2, "Image path"),
+		t({ "' />", "</div>" }),
+	}),
 }
